89e7c02047ce40d41a2dd1bd0a320ccc0c6759bb
Hiroki Yamamoto <futsalhiro@gmail.com>
2016-12-19 12:56:48 -0800

    課題の再提出
    課題・発展課題ともに改善しました

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boss {        
-	private int hp = 100;
+	public int hp = 100;
 	private int power = 25;
 	private int mp = 53;
 
@@ -14,17 +14,14 @@ public class Boss {
 		Debug.Log( damage+"のダメージを受けた" );
 		this.hp -= damage;
 	}
-
-	//発展課題の部分で行き詰まっています。
 	public void Magic() {
-		int c = this.mp - 5;
-		if (c >= 5) {
-				Debug.Log ("魔法攻撃をした。残りMPは" + c);
+		if (mp >= 5) {
+			    this.mp -= 5;
+				Debug.Log ("魔法攻撃をした。残りMPは" + mp);
 		} else {
 				Debug.Log("MPが足りないため魔法が使えない。");
 		}
-		return c;
-  }
+}
 }
 	
 public class Test : MonoBehaviour {
@@ -35,11 +32,15 @@ public class Test : MonoBehaviour {
 
 		    lastboss.Attack();
 		    lastboss.Defence(3);
-		    lastboss.Magic();
-		    
-		    //下記が課題です
-			int [] array = {10, 25, 76, 39, 66};
+		    //for文、１１回まわすと良い
+		    for (int i = 0; i<=10; i++) {
+		        lastboss.Magic();
+		    }
 
+			int [] array = {10, 25, 76, 39, 66};
+		    for (int i = 0; i < array.Length; i++) {
+			    Debug.Log(array [i]);
+		    }
 			for (int i = 4; i >= 0; i--) {
 				Debug.Log(array [i]);
 			}
@@ -47,6 +48,7 @@ public class Test : MonoBehaviour {
 	}
 	
 	void Update () {
-		
+	
 	}
+
 }
